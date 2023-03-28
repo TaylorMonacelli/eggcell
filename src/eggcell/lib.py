98f672205f1754fb00c12425b2cc39b08ee6b926
Taylor Monacelli <taylormonacelli@gmail.com>
2023-03-28 15:46:56 -0700

    Get rid of fuzzy matching templates, thats stupid

--- src/eggcell/lib.py
+++ src/eggcell/lib.py
@@ -3,7 +3,6 @@ import pathlib
 
 import jinja2
 import pkg_resources
-import rapidfuzz
 
 _logger = logging.getLogger(__name__)
 
@@ -11,27 +10,6 @@ package = __name__.split(".")[0]
 TEMPLATES_PATH = pathlib.Path(pkg_resources.resource_filename(package, "templates/"))
 loader = jinja2.FileSystemLoader(searchpath=TEMPLATES_PATH)
 env = jinja2.Environment(loader=loader, keep_trailing_newline=True)
-
-
-def fuzzy_match_template(template_name: str) -> str:
-    myglob = pathlib.Path(TEMPLATES_PATH).glob("*")
-    template_paths = list(myglob)
-
-    template_names = sorted([path.name for path in template_paths])
-
-    _logger.debug(template_names)
-
-    scores = []
-    for path in template_paths:
-        ratio = rapidfuzz.fuzz.ratio(template_name, str(path))
-        scores.append(ratio)
-
-    max_score = max(scores)
-    max_index = scores.index(max_score)
-    _logger.debug(template_paths[max_index])
-
-    template_fname = template_paths[max_index].name
-    return template_fname
 
 
 def main(args):
