89a059529372d887f6bcc9c4b94609958993c1ab
Taylor Monacelli <taylormonacelli@gmail.com>
2023-03-28 18:32:17 -0700

    refactor

--- src/eggcell/lib.py
+++ src/eggcell/lib.py
@@ -1,3 +1,4 @@
+import dataclasses
 import io
 import logging
 import pathlib
@@ -13,28 +14,65 @@ loader = jinja2.FileSystemLoader(searchpath=TEMPLATES_PATH)
 env = jinja2.Environment(loader=loader, keep_trailing_newline=True)
 
 
-def show_shell(shell: str, shells: list[str]):
+@dataclasses.dataclass
+class Shell:
+    name: str
+    want: bool
+    extension: str
+
+
+def want_shell(shell_name: str, shells: list[str]):
     for shell_iter in shells:
-        if shell in shell_iter.lower():
+        if shell_name in shell_iter.lower():
             return True
     return False
 
 
-def get_template_names(template_fnames: list[str], shells: list[str]) -> list[str]:
-    want_bash = show_shell("bash", shells)
-    want_powershell = show_shell("powershell", shells)
+def get_shell_preferences(shell_names: list[str]) -> bool:
+    shells = []
+
+    name = "bash"
+    s = Shell(name, want_shell(name, shell_names), extension="sh")
+    shells.append(s)
+
+    name = "powershell"
+    s = Shell(name, want_shell(name, shell_names), extension="ps1")
+    shells.append(s)
+
+    return shells
+
+
+def get_template_names(
+    shells: list[Shell], template_fnames: list[str], shell_names: list[str]
+) -> list[str]:
 
     filtered_template_fnames = []
-    for fname in template_fnames:
-        path = pathlib.Path(fname)
-        path_tmp = path.stem.replace(".j2", "")
-        _logger.debug(f"{path_tmp=}")
-        if want_bash and path_tmp.lower().endswith("sh"):
-            filtered_template_fnames.append(fname)
-        if want_powershell and path_tmp.lower().endswith("ps1"):
-            filtered_template_fnames.append(fname)
+    for shell in shells:
+        _logger.debug(type(shell))
+        for fname in template_fnames:
+            path = pathlib.Path(fname)
+            path_tmp = path.stem.replace(".j2", "")
+            _logger.debug(f"{path_tmp=}")
+
+            if shell.want and path_tmp.lower().endswith(shell.extension):
+                filtered_template_fnames.append(fname)
 
     return filtered_template_fnames
+
+
+def render_templates_with_header(templates: list[str], template_data: dict) -> str:
+    vfile = io.StringIO()
+    vfile.write("#" + "-" * 10)
+    vfile.write("\n")
+    for fname in templates:
+        template = env.get_template(fname)
+        rendered = template.render(data=template_data)
+        trimmed = rendered.strip()
+        out = f"{trimmed}\n"
+        vfile.write(out)
+        vfile.write("\n")
+
+    return vfile.getvalue()
 
 
 def main(args):
@@ -46,19 +84,13 @@ def main(args):
         "keychain.sh.j2",
     ]
 
-    shells = args.shells
-    filtered_template_fnames = get_template_names(template_fnames, shells)
-
-    vfile = io.StringIO()
-    vfile.write("#" + "-" * 10)
-    vfile.write("\n")
-    for fname in filtered_template_fnames:
-        template = env.get_template(fname)
-        rendered = template.render(data={"variables": args.variables})
-        trimmed = rendered.strip()
-        out = f"{trimmed}\n"
-        vfile.write(out)
-        vfile.write("\n")
-
-    print(vfile.getvalue(), end="")
+    shell_names = args.shells
+    shells = get_shell_preferences(shell_names)
+    filtered_template_fnames = get_template_names(shells, template_fnames, shell_names)
+    user_variables = args.variables
+    data = {"variables": user_variables}
+    out = render_templates_with_header(
+        templates=filtered_template_fnames, template_data=data
+    )
+    print(out)
     _logger.info("Script ends here")
